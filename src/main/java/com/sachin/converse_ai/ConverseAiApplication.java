1f32cb5b950b61d91db2224b5bc9e82c0e2293b0
Sachin Rajendra Mane <srm@Sachins-MacBook-Pro.local>
2026-04-01 00:54:05 +0530

    chore: initial project setup with Spring Boot starter

---
+++ src/main/java/com/sachin/converse_ai/ConverseAiApplication.java
@@ -0,0 +1,13 @@
+package com.sachin.converse_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverseAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverseAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachin/converse_ai/ConverseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin.converse_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverseAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
